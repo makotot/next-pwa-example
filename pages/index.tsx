1258f004e08da0c6ed1d07bd5171832f3bbdd066
makotot <makoto.ttn@gmail.com>
2020-06-19 02:50:57 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+
+const Index = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
+      const workbox = (window as any).workbox;
+      workbox.addEventListener('installed', (e) => {
+        console.log(`${e.type} triggered`)
+      })
+
+      workbox.addEventListener('controlling', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      workbox.addEventListener('activated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      workbox.addEventListener('waiting', event => {
+        console.log('waiting')
+        if (confirm('A new version is installed, reload to use the new version immediately?')) {
+          workbox.addEventListener('controlling', event => {
+            window.location.reload()
+          })
+          workbox.messageSW({type: 'SKIP_WAITING'})
+        } else {
+          // User rejected, new verion will be automatically load when user open the app next time.
+        }
+      })
+      workbox.register()
+    }
+  }, [])
+
+  return (
+    <div>
+      <div>next pwa..</div>
+      <div>next pwa..</div>
+    </div>
+  )
+}
+
+export default Index
