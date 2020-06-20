e141b4014813f15abf31c7f622a7c1e113cab49d
makotot <makoto.ttn@gmail.com>
2020-06-21 02:09:09 +0900

    isUpdate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,38 +4,38 @@ import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 
 const MyApp = ({ Component, pageProps }) => {
-  useEffect(() => {
-    console.log('useeffect')
-    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
-      const workbox = (window as any).workbox;
-      workbox.addEventListener('installed', (e) => {
-        console.log(`${e.type} triggered`)
-      })
+  // useEffect(() => {
+  //   console.log('useeffect')
+  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
+  //     const workbox = (window as any).workbox;
+  //     workbox.addEventListener('installed', (e) => {
+  //       console.log(`${e.type} triggered`)
+  //     })
 
-      workbox.addEventListener('controlling', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
+  //     workbox.addEventListener('controlling', event => {
+  //       console.log(`Event ${event.type} is triggered.`)
+  //       console.log(event)
+  //     })
 
-      workbox.addEventListener('activated', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
+  //     workbox.addEventListener('activated', event => {
+  //       console.log(`Event ${event.type} is triggered.`)
+  //       console.log(event)
+  //     })
 
-      workbox.addEventListener('waiting', event => {
-        console.log('waiting')
-        if (confirm('A new version is installed, reload to use the new version immediately?')) {
-          workbox.addEventListener('controlling', event => {
-            window.location.reload()
-          })
-          workbox.messageSW({type: 'SKIP_WAITING'})
-        } else {
-          // User rejected, new verion will be automatically load when user open the app next time.
-        }
-      })
-      workbox.register()
-    }
-  }, [])
+  //     workbox.addEventListener('waiting', event => {
+  //       console.log('waiting')
+  //       if (confirm('A new version is installed, reload to use the new version immediately?')) {
+  //         workbox.addEventListener('controlling', event => {
+  //           window.location.reload()
+  //         })
+  //         workbox.messageSW({type: 'SKIP_WAITING'})
+  //       } else {
+  //         // User rejected, new verion will be automatically load when user open the app next time.
+  //       }
+  //     })
+  //     workbox.register()
+  //   }
+  // }, [])
 
   return (
     <ThemeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,55 @@ import Head from 'next/head'
 import { APP_TITLE } from '../config'
 
 const Index = () => {
+  useEffect(() => {
+    console.log('useeffect')
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
+      console.log('sw------')
+      const workbox = (window as any).workbox;
+      workbox.addEventListener('installed', (e) => {
+        console.log(`${e.type} triggered`)
+        console.log(e.isUpdate)
+        if (e.isUpdate) {
+          if (confirm('A new version is installed, reload to use the new version immediately?')) {
+            window.location.reload()
+            workbox.messageSW({type: 'SKIP_WAITING'})
+          }
+        }
+      })
+
+      workbox.addEventListener('controlling', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      workbox.addEventListener('activated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      // workbox.addEventListener('waiting', event => {
+      //   console.log('waiting')
+      //   if (confirm('A new version is installed, reload to use the new version immediately?')) {
+      //     workbox.addEventListener('controlling', event => {
+      //       window.location.reload()
+      //     })
+      //     workbox.messageSW({type: 'SKIP_WAITING'})
+      //   } else {
+      //     // User rejected, new verion will be automatically load when user open the app next time.
+      //   }
+      // })
+      workbox.register()
+    }
+  }, [])
   return (
     <>
       <Head>
         <title>{ APP_TITLE } / index</title>
       </Head>
       <div>
+        <Box>box</Box>
+        <Box>box</Box>
+        <Box>box</Box>
         <Box>box</Box>
         <div>next pwa..</div>
         <div>next pwa..</div>
