c6e227eb18faccbeebe257200d4b30d60f546f8a
makotot <makoto.ttn@gmail.com>
2020-06-21 11:11:56 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,60 @@
 import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from '@chakra-ui/core'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 
+const useSW = () => {
+  useEffect(() => {
+    console.log('useeffect')
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
+      console.log('sw------')
+      const workbox = (window as any).workbox;
+      workbox.addEventListener('installed', (e) => {
+        console.log(`${e.type} triggered`)
+        console.log(e.isUpdate)
+        if (e.isUpdate) {
+          if (confirm('A new version is installed, reload to use the new version immediately?')) {
+            window.location.reload()
+            workbox.messageSW({type: 'SKIP_WAITING'})
+          }
+        }
+      })
+
+      workbox.addEventListener('controlling', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      workbox.addEventListener('activated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      workbox.register()
+    }
+  }, [])
+}
+
+const useUpdateSWonRouteChange = () => {
+  const router = useRouter()
+  const updateSW = () => {
+    navigator.serviceWorker.ready.then((registration) => {
+      console.log('update sw')
+      registration.update()
+    })
+  }
+  useEffect(() => {
+    router.events.on('routerChangeComplete', updateSW)
+    return () => {
+      router.events.off('routerChangeComplete', updateSW)
+    }
+  }, [])
+}
+
 const MyApp = ({ Component, pageProps }) => {
-  // useEffect(() => {
-  //   console.log('useeffect')
-  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
-  //     const workbox = (window as any).workbox;
-  //     workbox.addEventListener('installed', (e) => {
-  //       console.log(`${e.type} triggered`)
-  //     })
-
-  //     workbox.addEventListener('controlling', event => {
-  //       console.log(`Event ${event.type} is triggered.`)
-  //       console.log(event)
-  //     })
-
-  //     workbox.addEventListener('activated', event => {
-  //       console.log(`Event ${event.type} is triggered.`)
-  //       console.log(event)
-  //     })
-
-  //     workbox.addEventListener('waiting', event => {
-  //       console.log('waiting')
-  //       if (confirm('A new version is installed, reload to use the new version immediately?')) {
-  //         workbox.addEventListener('controlling', event => {
-  //           window.location.reload()
-  //         })
-  //         workbox.messageSW({type: 'SKIP_WAITING'})
-  //       } else {
-  //         // User rejected, new verion will be automatically load when user open the app next time.
-  //       }
-  //     })
-  //     workbox.register()
-  //   }
-  // }, [])
+  useSW()
+  useUpdateSWonRouteChange()
 
   return (
     <ThemeProvider>
