c7d386a0e8941a8400b2812ac83ef18620e20244
makotot <makoto.ttn@gmail.com>
2020-06-21 20:50:22 +0900

    rm

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import { Badge, Box, Flex } from '@chakra-ui/core'
+import { Box, Flex } from '@chakra-ui/core'
 
 export const Header: React.FC = () => (
   <header>
     <h1>next pwa example</h1>
-    <Badge>badge</Badge>
-    <Badge>badge</Badge>
     <nav>
       <Flex>
         <Box p="1rem" backgroundColor="tomato">
