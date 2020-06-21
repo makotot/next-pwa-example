ec81a26e3b177db7cd39a2021e5a8a1356911a5f
makotot <makoto.ttn@gmail.com>
2020-06-21 20:42:47 +0900

    rm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,47 +1,13 @@
 import React, { useEffect } from 'react'
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from '@chakra-ui/core'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 
 const useSW = () => {
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator && (window as any).workbox !== 'undefined') {
-  //     const workbox = (window as any).workbox;
-
-  //     workbox.addEventListener('installed', (e) => {
-  //       console.log(`${e.type} triggered`)
-  //       console.log(e.isUpdate)
-  //       if (e.isUpdate) {
-  //         if (confirm('A new version is installed, reload to use the new version immediately?')) {
-  //           window.location.reload()
-  //           workbox.messageSW({type: 'SKIP_WAITING'})
-  //         }
-  //       }
-  //     })
-
-  //     workbox.addEventListener('waiting', event => {
-  //       console.log(`Event ${event.type} is triggered.`)
-  //       console.log(event)
-  //       console.log(event.isUpdate)
-  //     })
-
-  //     workbox.addEventListener('controlling', event => {
-  //       console.log(`Event ${event.type} is triggered.`)
-  //       console.log(event)
-  //     })
-
-  //     workbox.addEventListener('activated', event => {
-  //       console.log(`Event ${event.type} is triggered.`)
-  //       console.log(event)
-  //     })
-
-  //     workbox.register()
-  //   }
-  // }, [])
-
   useEffect(()=>{
     const workbox = (window as any).workbox;
+
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator && workbox !== undefined) {
       // add event listeners to handle any of PWA lifecycle event
       // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
@@ -101,7 +67,7 @@ const useUpdateSWonRouteChange = () => {
   const router = useRouter()
   const updateSW = () => {
     navigator.serviceWorker.ready.then((registration) => {
-      console.log('update sw')
+      console.log(registration)
       registration.update()
     })
   }
